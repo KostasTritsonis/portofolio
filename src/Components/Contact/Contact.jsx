c7bb7b09d8465b59009db55b9464fb903b337b9a
KostasTritsonis <ktritsonis@hotmail.com>
2024-12-13 17:18:28 +0200

    131224

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,15 +4,16 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <div className='contact'>
+    <form className='contact' action="https://formspree.io/f/xgvebbbq"
+    method="POST">
         <h1>CONTACT</h1>
         <div className="info">
             <input name='name'   type="text" placeholder='Name' required />
             <input name='email' type="text" placeholder='Email' required />
             <textarea name='message' type="text" placeholder='Message' ></textarea>
         </div>
-        <button>Submit</button>
-    </div>
+        <button type="submit">Submit</button>
+    </form>
   )
 }
 
