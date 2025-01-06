d989e973528841eec83311db1e4be22d03ed50c3
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-06 23:58:22 +0200

    06012025

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import './Contact.css'
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
+
+  const sendEmail = () => {
+    let params = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('message').value
+    }
+
+    emailjs.send('service_bqaq3j6', 'template_hmf47rj', params).then(alert('Email sent successfully!'))
+  }
+
   return (
-    <form className='contact' action="https://formspree.io/f/xgvebbbq"
-    method="POST">
+    <form className='contact'>
         <h1>CONTACT</h1>
         <div className="info">
-            <input name='name'   type="text" placeholder='Name' required />
-            <input name='email' type="text" placeholder='Email' required />
-            <textarea name='message' type="text" placeholder='Message' ></textarea>
+            <input name='name' id="name"   type="text" placeholder='Name' required />
+            <input name='email' id="email" type="text" placeholder='Email' required />
+            <textarea name='message' id="message" type="text" placeholder='Message' ></textarea>
         </div>
-        <button type="submit">Submit</button>
+        <button onClick={sendEmail} type="submit">Submit</button>
     </form>
   )
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import './Projects.css'
 import movie from '../../assets/movie.png'
 import compiler from '../../assets/compiler.png'
-import hipster from '../../assets/hipsster.png'
-import edusity from '../../assets/edusity.png'
+import enginuity from '../../assets/enginuity.png'
 import lift from '../../assets/fitlift.png'
 import ProjectCard from '../ProjectCard/ProjectCard'
 
@@ -12,12 +11,12 @@ const Projects = () => {
     <div className='projects'>
       <h1>PROJECTS</h1>
       <h2>FRONT END</h2>
-      <div className='project1'>
-        <ProjectCard src={edusity} link="https://edusity-snowy.vercel.app/" name="Edusity" title="University Website"/>
+      <div className='project'>
+        <ProjectCard src={enginuity} link="https://matina-snowy.vercel.app/" name="Enginuity" title="English Class Website"/>
         <ProjectCard src={lift} link="" name="FitLift" title="Gym Website"/>
       </div>
       <h2>BACK END</h2>
-      <div className='project2'>
+      <div className='project'>
         <ProjectCard src={movie} className='movie' link="https://github.com/KostasTritsonis/information_retrieval" name="MSearcher " title="Movie Search App"/>
         <ProjectCard src={compiler} link="https://github.com/KostasTritsonis/compiler-UI" name="Compiler" title="Compiler UI App"/>
       </div>
