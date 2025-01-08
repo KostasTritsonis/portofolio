85ed088a731c1ca25b53458ac08b28b65ecfad18
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-08 13:37:28 +0200

    07012025

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Hero.css';
 import heroImg from '../../assets/hero-img.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
@@ -11,31 +10,29 @@ import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
 import { useTheme } from '../Context/Context';
 
+
 const Hero = () => {
-
     const {theme,toggleTheme} = useTheme();
-
-    const themeIcon = theme === 'light' ? sun : moon;
-    const facebookIcon = theme === 'light' ? facebookLight : facebookDark;
-    const githubIcon = theme === 'light' ? githubLight : githubDark;
-    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+    const [iconFacebook,iconGithub,iconLinkedIn,iconTheme] = theme === 'light' ?
+    [facebookLight,githubLight,linkedinLight,sun] :
+    [facebookDark,githubDark,linkedinDark,moon];
 
   return (
     <div className='hero'>
         <div className='hero-right'>
             <img src={heroImg} alt="Profile picture" />
-            <img className='colorMode'  src={themeIcon} alt="" onClick={toggleTheme}/>
+            <img className='colorMode' src={iconTheme} alt="" onClick={toggleTheme}/>
         </div>
         <div className='hero-left'>
             <h1>KONSTANTINOS <br /> TRITSONIS</h1>
             <h2>Software Developer</h2>
             <span>
                 <a href="https://www.facebook.com/kwstas.tritsonis" aria-label="facebook" target='_blank'>
-                <img src={facebookIcon} alt="" /></a>
+                <img src={iconFacebook} alt="" /></a>
                 <a href="https://github.com/KostasTritsonis" aria-label="github" target='_blank'>
-                <img src={githubIcon} alt="" /></a>
+                <img src={iconGithub} alt="" /></a>
                 <a href="https://www.linkedin.com/in/kostantinos-tritsonis-637522214/" aria-label="linkedin" target='_blank'>
-                <img src={linkedinIcon} alt="" /></a>
+                <img src={iconLinkedIn} alt="" /></a>
             </span>
             <p>With a passion for developing modern apps</p>
             {/*<a href={CV} download>
